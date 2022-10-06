7204c6bbb1cfc9ab2d34b0113eae2098b8f86e9a
shisaj7891 <shisaj7891@gmail.com>
2022-10-07 02:17:49 +0530

    second commit

--- ecommerce_full/lib/food_page_body.dart
+++ ecommerce_full/lib/food_page_body.dart
@@ -8,11 +8,14 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.amber,
       height: 250,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return buildPageItem(position);
@@ -21,14 +24,28 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget buildPageItem(int index) {
-    return Container(
-      height: 150,
-      margin: EdgeInsets.only(left: 10, right: 10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: index.isEven ? Color(0xFF69c5DF) : Color(0xFF9294cc),
-          image: DecorationImage(
-              image: AssetImage('assets/images/jewel.jpg'), fit: BoxFit.cover)),
-    );
+    return Stack(children: [
+      Container(
+        height: 170,
+        margin: EdgeInsets.only(left: 5, right: 5, top: 5),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: index.isEven ? Color(0xFF69c5DF) : Color(0xFF9294cc),
+            image: DecorationImage(
+                image: AssetImage('assets/images/jewel.jpg'),
+                fit: BoxFit.cover)),
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 100,
+          margin: EdgeInsets.only(left: 25, right: 25, bottom: 18),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ]);
   }
 }
